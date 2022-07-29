f3ab92dd364005d645f5b3c20d90ea0964888548
Joe Boyle <joe.boyle@blackcowtech.uk>
2022-07-29 12:51:05 +0100

    Refactor naming etc

--- BloomFilter.c
+++ BloomFilter.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 #include <stdbool.h>
-enum
-{
-    N = 4,    // the number of strings
-    T = 8,    // the number of test words
-    M = 1000, // the size of the bloom filter
-    k = 3,    // the number of hash functions
-};
-int modularHash(const char string[], int R, int M);
-int checkString(const int bloomFilter[M], const char string[]);
-void addString(int bloomFilter[M], const char string[]);
+
+#define N 4    // the number of strings
+#define T 8    // the number of test words
+#define C 4    // the number of characters in the words
+#define M 1000 // the size of the bloom filter
+#define k 3    // the number of hash functions
 
 // Each pair of i'th elements is a seed pair for a 'unique' hash function.
-int RprimeSeeds[k] = {1103, 761, 83};
-int MprimeSeeds[k] = {173, 587, 101};
+int prime_seeds_1[k] = {1103, 761, 83};
+int prime_seeds_2[k] = {173, 587, 97};
 
-const char *sampleWords[N] = {"fizz", "buzz", "bonk", "zzzz"};
-const char *testWords[T] = {
-    "fizz",
-    "buzz",
-    "bonk",
-    "zzzz",
-    "yyyy", // not in the bloom filter
-    "yike", // not in the bloom filter
-    "zzyz", // not in the bloom filter
-    "wyzz", // not in the bloom filter
-};
+int modularHash(const char string[], int seed_1, int seed_2);
+int checkString(const int bloomFilter[M], const char string[]);
+void addString(int bloomFilter[M], const char string[]);
 
 int main()
 {
 
-    int bloomFilter[M] = {0}; // array of zeroes.
+    const char *sampleWords[N] = {"fizz", "buzz", "bonk", "zzzz"};
+    const char *testWords[T] = {
+        // in the bloom filter
+        "fizz",
+        "buzz",
+        "bonk",
+        "zzzz",
+        // not in the bloom filter
+        "yyyy",
+        "yike",
+        "yzzz",
+        "zzyz",
+    };
 
+    int bloomFilter[M] = {0}; // array of zeroes.
     for (int i = 0; i < N; i++)
     {
         addString(bloomFilter, sampleWords[i]);
@@ -41,9 +42,10 @@ int main()
     {
         printf("%d", bloomFilter[i]);
     }
+    printf("\n");
     for (int i = 0; i < T; i++)
     {
-        printf("%d\n", checkString(bloomFilter, testWords[i]));
+        printf("word: '%s' bloom filter check result: %d\n", testWords[i], checkString(bloomFilter, testWords[i]));
     }
 }
 
@@ -55,7 +57,7 @@ void addString(int bloomFilter[M], const char string[])
 {
     for (int i = 0; i < k; i++)
     {
-        int hash = modularHash(string, RprimeSeeds[i], MprimeSeeds[i]);
+        int hash = modularHash(string, prime_seeds_1[i], prime_seeds_2[i]);
         int position = hash % M;
         bloomFilter[position] = 1;
     }
@@ -69,7 +71,7 @@ int checkString(const int bloomFilter[M], const char string[])
 {
     for (int i = 0; i < k; i++)
     {
-        int hash = modularHash(string, RprimeSeeds[i], MprimeSeeds[i]);
+        int hash = modularHash(string, prime_seeds_1[i], prime_seeds_2[i]);
         int position = hash % M;
 
         if (bloomFilter[position] == 1)
@@ -85,16 +87,14 @@ int checkString(const int bloomFilter[M], const char string[])
  * @param R: a seed value, should be a prime
  * @param M: a seed value, should also be a prime
  */
-int modularHash(const char string[], int R, int M)
+int modularHash(const char string[], int seed_1, int seed_2)
 {
-    int chars = 4; // the number of characters
     int hash = 0;
-
-    for (int i = 0; i < chars; i++)
+    for (int i = 0; i < C; i++)
     {
         char s = string[i];
         int n = (int)s;
-        hash = (R * hash + n) % M;
+        hash = (seed_1 * hash + n) % seed_2;
     }
     return hash;
 }
