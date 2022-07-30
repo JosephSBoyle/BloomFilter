75156e71984a4c32395db9997a37798fd9d312e6
Joe Boyle <joe.boyle@blackcowtech.uk>
2022-07-30 08:39:55 +0100

    Add a help string to the CLI interface and a README file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Bloomfilter
+
+This project is a simple `C` implementation of a [bloom filter](https://en.wikipedia.org/wiki/Bloom_filter)
+with a basic CLI interface.
+
+## Quickstart
+To run the project, build and run the executeable:
+
+```bash
+$ make
+$ ./main
+```
--- main.c
+++ main.c
@@ -8,7 +8,13 @@ int main(int argc, char *argv[])
 {
     if (argc < 3)
     {
-        printf("Error: at least 2 args are required.\n");
+        printf("Error: at least 2 args are required.\n\n"
+               "Args\n"
+               "    arg: [*filter_string]: a sequence of strings to add to the bloom filter.\n"
+               "    arg: test_word: a word to check for in the filter\n\n"
+               "Usage\n"
+               "$ ./main [*filter_string] test_string\n"
+               "$ ./main filter_string_1 filter_string_2 ... filter_string_n test_word\n\n");
         return 1;
     }
     int bloom_filter[M] = {0};
@@ -21,6 +27,5 @@ int main(int argc, char *argv[])
     char *last_string = argv[argc - 1];
     bool match = check_string(bloom_filter, last_string);
     printf("Is the last string: '%s' in the set of first strings?\n", last_string);
-
     (match == true) ? printf("probably\n") : printf("definitely not\n");
 }
